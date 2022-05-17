ff1023a8cf21c20e71971844eacbd4434895fbfe
CENGIZHAN KARABEL <cengizhankarabel@gmail.com>
2022-05-16 22:45:53 -0500

    transfer-service-v1 tightly coupled

--- transfer-service-v1/src/main/java/com/example/repository/JpaAccountRepository.java
+++ transfer-service-v1/src/main/java/com/example/repository/JpaAccountRepository.java
@@ -5,21 +5,19 @@ import com.example.model.Account;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class JdbcAccountRepository implements AccountRepository {
-
-	public JdbcAccountRepository() {
-		log.info("JdbcAccountRepository instance created..");
+public class JpaAccountRepository implements AccountRepository {
+	
+	public JpaAccountRepository() {
+		log.info("JpaAccountRepository instance created..");
 	}
 
 	public Account loadAccount(String number) {
-
 		log.info("loading account.." + number);
 
 		return new Account(number, 1000.00);
 	}
 
 	public void updateAccount(Account account) {
-
 		log.info("updating account.." + account.getNumber());
 
 	}
--- transfer-service-v1/src/main/java/com/example/service/TransferServiceImpl.java
+++ transfer-service-v1/src/main/java/com/example/service/TransferServiceImpl.java
@@ -1,23 +1,23 @@
 package com.example.service;
 
-
 import com.example.model.Account;
+import com.example.repository.AccountRepository;
 import com.example.repository.JdbcAccountRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class TransferServiceImpl {
-	
-	public TransferServiceImpl() {
+public class TransferServiceImpl implements TransferService {
+
+	private AccountRepository accountRepository;
+
+	public TransferServiceImpl(AccountRepository accountRepository) {
+		this.accountRepository = accountRepository;
 		log.info("TransferServiceImpl instance created...");
 	}
-	
 
 	public void transfer(double amount, String fromAccNum, String toAccNum) {
 		log.info("transfer initiated..");
-
-		JdbcAccountRepository accountRepository = new JdbcAccountRepository();
 
 		Account fromAccount = accountRepository.loadAccount(fromAccNum);
 		Account toAccount = accountRepository.loadAccount(toAccNum);
@@ -27,7 +27,7 @@ public class TransferServiceImpl {
 
 		accountRepository.updateAccount(fromAccount);
 		accountRepository.updateAccount(toAccount);
-		
+
 		log.info("transfer finished..");
 
 	}
